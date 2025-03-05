5d18803d894d4d05f7a2704f392ca0e02043d89b
littelamaterasu <clgtthangtung@gmail.com>
2025-03-05 08:39:36 +0700

    init test branch

---
+++ assets/scripts/ForTest.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('ForTest')
+export class ForTest extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
